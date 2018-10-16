b79042ed5bab74d03e95d851d51e3d413814ef16
Nader Ghanbari <nader.ghanbari@yoppworks.com>
2018-10-16 01:08:45 -0400

    - More tests for Abstraction:   - Exercise 2.22.b  - [x].x(Sy) ≡ SI(K(Sy))    ∀ x, y   - Exercise 2.22.c  - [x].uxxv ≡ S(SuI)(Kv)    ∀ u, v, x

--- src/test/scala/cl/AbstractionTest.scala
+++ src/test/scala/cl/AbstractionTest.scala
@@ -8,7 +8,7 @@ import org.scalatest.{ Matchers, WordSpec }
 
 class AbstractionTest extends WordSpec with Matchers {
 
-  "Example 2.19   ---        [x].xy ≡ SI(Ky)           ∀ x, y" in
+  "Example 2.19    ---        [x].xy ≡ SI(Ky)           ∀ x, y" in
     ∀(varGen, varGen) { (x, y) =>
       whenever(x != y) {
         val left = |(x)| (x $ y)
@@ -17,19 +17,19 @@ class AbstractionTest extends WordSpec with Matchers {
       }
     }
 
-  "Theorem 2.21.A ---        [x].M exists              ∀ M, x" in
+  "Theorem 2.21.A  ---        [x].M exists              ∀ M, x" in
     ∀(varGen, termGen) { (x, M) =>
       |(x)| M should not be null
     }
 
-  "Theorem 2.21.B ---        ([x].M)N ▹w [N/x]M        ∀ M, N, x" in
+  "Theorem 2.21.B  ---        ([x].M)N ▹w [N/x]M        ∀ M, N, x" in
     ∀(varGen, termGen, termGen) { (x, M, N) =>
       val left = |(x)| M $ N
       val right = (N / x) (M)
       reduceToWeakNormalForm(left) shouldEqual reduceToWeakNormalForm(right)
     }
 
-  "Theorem 2.21.B ---        ([x].M)N ▹w [N/x]M        ∀ M, N, x ∈ FV(M)" in
+  "Theorem 2.21.B  ---        ([x].M)N ▹w [N/x]M        ∀ M, N, x ∈ FV(M)" in
     ∀(termGen, termGen) { (M, N) =>
       whenever(M.FV.nonEmpty) {
         val x = M.FV.head
@@ -39,13 +39,13 @@ class AbstractionTest extends WordSpec with Matchers {
       }
     }
 
-  "Theorem 2.21.C ---        x ∉ FV([x].M)             ∀ M, x" in
+  "Theorem 2.21.C  ---        x ∉ FV([x].M)             ∀ M, x" in
     ∀(varGen, termGen) { (x, M) =>
       val abstracted = |(x)| M
       abstracted.FV should not contain x
     }
 
-  "Theorem 2.21.C ---        x ∉ FV([x].M)             ∀ M, x ∈ FV(M)" in
+  "Theorem 2.21.C  ---        x ∉ FV([x].M)             ∀ M, x ∈ FV(M)" in
     ∀(termGen) { M =>
       whenever(M.FV.nonEmpty) {
         val x = M.FV.head
@@ -54,11 +54,29 @@ class AbstractionTest extends WordSpec with Matchers {
       }
     }
 
-  "Exercise 2.22   ---        [x].u(vx) ≡ S(Ku)v       ∀ u, v, x" in
+  "Exercise 2.22.a ---        [x].u(vx) ≡ S(Ku)v        ∀ u, v, x" in
     ∀(varGen, varGen, varGen) { (u, v, x) =>
       whenever(u != v && u != x && x != v) {
         val left = |(x)| (u $ (v $ x))
         val right = S $ (K $ u) $ v
+        left shouldEqual right
+      }
+    }
+
+  "Exercise 2.22.b ---        [x].x(Sy) ≡ SI(K(Sy))     ∀ x, y" in
+    ∀(varGen, varGen) { (x, y) =>
+      whenever(x != y) {
+        val left = |(x)| (x $ (S $ y))
+        val right = S $ I $ (K $ (S $ y))
+        left shouldEqual right
+      }
+    }
+
+  "Exercise 2.22.c ---        [x].uxxv ≡ S(SuI)(Kv)     ∀ u, v, x" in
+    ∀(varGen, varGen, varGen) { (u, v, x) =>
+      whenever(u != v && u != x && x != v) {
+        val left = |(x)| (u $ x $ x $ v)
+        val right = S $ (S $ u $ I) $ (K $ v)
         left shouldEqual right
       }
     }
