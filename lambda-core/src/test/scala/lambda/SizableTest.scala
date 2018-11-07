a9101a82ddbd3e72f9559191ab1e417d92b96103
Nader Ghanbari <nader.ghanbari@yoppworks.com>
2018-11-07 00:45:56 -0500

    Simple test for lambda.Sizable

---
+++ lambda-core/src/test/scala/lambda/SizableTest.scala
@@ -0,0 +1,20 @@
+package lambda
+
+import LambdaGen.termGen
+import org.scalatest.prop.GeneratorDrivenPropertyChecks.{forAll => ∀}
+import org.scalatest.{Matchers, WordSpec}
+
+class SizableTest extends WordSpec with Matchers {
+
+  "Lambda Term length (lgh) is a non-negative function" in ∀(termGen) { M ⇒
+    val length = M.length
+    length should be > 0
+  }
+
+  "Lambda Term length (lgh) is increasing and commutative wrt Application ($)" in ∀(termGen, termGen) { (U, V) ⇒
+    U.length       should be < $(U, V).length
+    U.length       should be < $(V, U).length
+    $(U, V).length shouldEqual $(V, U).length
+  }
+
+}
