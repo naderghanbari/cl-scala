e2ffb0df150a08a524fc63b43166ba57dc5316dc
Nader Ghanbari <nader.ghanbari@yoppworks.com>
2018-11-21 23:44:22 -0500

    #6 JLineSupport Naming improvements

--- cl-repl/src/main/scala/cl/repl/JLineSupport.scala
+++ cl-repl/src/main/scala/cl/repl/JLineSupport.scala
@@ -2,9 +2,9 @@ package cl.repl
 
 import java.util
 
-import org.jline.reader.impl.completer.StringsCompleter
 import org.jline.reader._
-import org.jline.terminal.{Terminal, TerminalBuilder}
+import org.jline.reader.impl.completer.StringsCompleter
+import org.jline.terminal.TerminalBuilder
 
 import scala.collection.JavaConverters.asJavaIterable
 import scala.io.AnsiColor._
@@ -12,26 +12,26 @@ import scala.io.AnsiColor._
 trait JLineSupport {
 
   def state: ReplStateMachine.State
+  private lazy val completer                        = new StateContextCompleter(state)
+  def updateJLine(newState: ReplStateMachine.State) = completer.updateState(newState)
 
-  private lazy val completer = new DelegateCompleter(new StringsCompleter(asJavaIterable(state.ρ.refs.keys)))
-  def updateJLine(newState: ReplStateMachine.State) =
-    completer.setCompleter(new StringsCompleter(asJavaIterable(state.ρ.refs.keys)))
+  private lazy val terminal = TerminalBuilder.terminal()
+  private lazy val reader   = LineReaderBuilder.builder().terminal(terminal).completer(completer).build()
 
-  private lazy val terminal: Terminal = TerminalBuilder.terminal()
-  private lazy val reader: LineReader = LineReaderBuilder.builder().terminal(terminal).completer(completer).build()
-
-  def prompt: String = s"${CYAN}CL ${state.system.name} > $RESET"
-
+  def prompt: String           = s"${CYAN}CL ${state.system.name} > $RESET"
   def readCommand(): String    = reader.readLine(prompt)
   def put(s: String): Unit     = terminal.writer().print(s)
   def putLine(s: String): Unit = terminal.writer().println(s)
+  def `<u>`(s: String)         = s"$UNDERLINED$s$RESET"
 
-  def `<u>`(s: String) = s"$UNDERLINED$s$RESET"
+}
 
-  class DelegateCompleter(var delegate: Completer) extends Completer {
-    def setCompleter(delegate: Completer): Unit = this.delegate = delegate
-    override def complete(reader: LineReader, line: ParsedLine, candidates: util.List[Candidate]): Unit =
-      delegate.complete(reader, line, candidates)
-  }
+class StateContextCompleter(state: ReplStateMachine.State) extends Completer {
+  var delegate = new StringsCompleter(asJavaIterable(state.ρ.refs.keys))
 
+  def updateState(state: ReplStateMachine.State): Unit =
+    delegate = new StringsCompleter(asJavaIterable(state.ρ.refs.keys))
+
+  override def complete(reader: LineReader, line: ParsedLine, candidates: util.List[Candidate]): Unit =
+    delegate.complete(reader, line, candidates)
 }
