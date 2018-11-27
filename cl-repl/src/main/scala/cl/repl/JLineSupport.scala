a23c9b184eeee543f52bd40aa8bdd236cf5fcd17
Nader Ghanbari <nader.ghanbari@yoppworks.com>
2018-11-27 10:54:57 +0800

    Prevent JLine line reader from escaping characters

--- cl-repl/src/main/scala/cl/repl/JLineSupport.scala
+++ cl-repl/src/main/scala/cl/repl/JLineSupport.scala
@@ -1,5 +1,6 @@
 package cl.repl
 
+import org.jline.reader.LineReader.Option.{DISABLE_EVENT_EXPANSION => NoEscape}
 import org.jline.reader._
 import org.jline.terminal.TerminalBuilder
 
@@ -8,16 +9,23 @@ import scala.io.AnsiColor._
 trait JLineSupport {
 
   def state: ReplStateMachine.State
-  private lazy val completer                        = new StateContextCompleter(state)
-  def updateJLine(newState: ReplStateMachine.State) = completer.updateState(newState)
 
-  private lazy val terminal = TerminalBuilder.terminal()
-  private lazy val reader   = LineReaderBuilder.builder().terminal(terminal).completer(completer).build()
+  private lazy val completer =
+    new StateContextCompleter(state)
+
+  def updateJLine(newState: ReplStateMachine.State) =
+    completer.updateState(newState)
+
+  private lazy val terminal =
+    TerminalBuilder.terminal()
+
+  private lazy val reader =
+    LineReaderBuilder.builder().terminal(terminal).completer(completer).option(NoEscape, true).build()
 
   def prompt: String           = s"${CYAN}CL ${state.system.name} > $RESET"
   def readCommand(): String    = reader.readLine(prompt).trim
   def put(s: String): Unit     = terminal.writer().print(s)
   def putLine(s: String): Unit = terminal.writer().println(s)
-  def `<u>`(s: String)         = s"$UNDERLINED$s$RESET"
+  def `<u>`(s: String): String = s"$UNDERLINED$s$RESET"
 
 }
